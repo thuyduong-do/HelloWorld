baf565ee15da56564a89e33b70821482a550bf03
thuy duong do <thuyduongdo98@gmail.com>
2019-09-15 21:06:37 +0200

    modify program file

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -10,7 +10,7 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("hello world");
+            Console.WriteLine("duong world");
         }
     }
 }
