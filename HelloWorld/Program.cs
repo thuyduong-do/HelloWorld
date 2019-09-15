adf70f68027ce567e6b2600f54624fe5c7cf8bca
thuy duong do <thuyduongdo98@gmail.com>
2019-09-15 21:13:16 +0200

    altered program

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -10,7 +10,18 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
+            int[] A = new int[3] { 1, 2, 3 };
+            
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine(A[i]);
+            }
+            foreach (var i in A)
+            {
+                Console.WriteLine(i);
+            }
             Console.WriteLine("duong world");
+            Console.ReadLine();
         }
     }
 }
